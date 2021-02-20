16b01cdbdb983c6cdfff944f46def15dc2e720fb
lucia2cv <lucia2cv98@gmail.com>
2021-02-20 13:08:58 +0100

    wip

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { TalleresComponent } from './components/talleres/talleres.component';
 import { EquiposComponent } from './components/equipos/equipos.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {ROUTES} from "./app.routes";
+import {MatTabsModule} from "@angular/material/tabs";
 
 
 
@@ -64,7 +65,7 @@ import {ROUTES} from "./app.routes";
     MatDialogModule,
     MatRadioModule,
     MatButtonModule,
-
+    MatTabsModule
 
   ],
   providers: [ LoginService,
